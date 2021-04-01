e1366eabd75be06bbdaf67bb889850e72f147cff
366day <923013941@qq.com>
2021-04-01 16:21:45 +0800

    备注

--- dyx/TestCase/test_登录功能.py
+++
@@ -1,9 +0,0 @@
-from dyx.web_ZhiNengYunWei.PageObject.LoginPage.LoginPage import Test_在登录界面
-
-
-def test_登录功能():
-    Test_在登录界面().test_打开浏览器()
-    Test_在登录界面().test_输入访问地址('http://10.136.107.175:7007/views/login.html')
-    Test_在登录界面().test_输入用户名()
-    Test_在登录界面().test_输入密码()
-    Test_在登录界面().test_点击登录按钮()
--- lcj/tool/get_yaml.py
+++
@@ -1,9 +0,0 @@
-""" yaml调用封装 """
-import yaml
-
-def get_yaml(filename):
-    data_file = '../yidong_web/IP_Intelligent_system/data/' + filename
-    with open(data_file,encoding='utf-8') as f:
-        return yaml.load(f,Loader=yaml.FullLoader)
-
-
